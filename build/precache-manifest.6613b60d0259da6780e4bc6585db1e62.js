3722da0cc624ed705db21d3e61b6e43e415bc2b4
sureyaF <71413221+sureyaF@users.noreply.github.com>
2020-10-15 21:41:29 -0400

    update error

---
+++ build/precache-manifest.6613b60d0259da6780e4bc6585db1e62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "690e0055a46e4d48d556",
+    "url": "/static/css/main.f7931580.chunk.css"
+  },
+  {
+    "revision": "690e0055a46e4d48d556",
+    "url": "/static/js/main.690e0055.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "aec1398a15e7ea1aeccf070647479d90",
+    "url": "/index.html"
+  }
+];
